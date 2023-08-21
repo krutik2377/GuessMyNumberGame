a3574e29836a0bfcc496cb432df955675e22a2a9
krutik2377 <krutikgevariya7723@gmail.com>
2023-08-21 17:50:05 -0400

    All the coditions added for guess comparision

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 'use strict';
+
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number';
 
@@ -6,4 +8,28 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 21;
 
 console.log(document.querySelector('.guess').value);
-document.querySelector('.guess').value = 23;
+document.querySelector('.guess').value = 23; */
+
+const SecretNumber = Math.trunc(Math.random()*20) +1;
+
+document.querySelector('.number').textContent = SecretNumber;
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    
+    if(!guess){
+        document.querySelector('.message').textContent = 'No Number';
+    }
+    else if(guess === SecretNumber)
+    {
+        document.querySelector('.message').textContent = "Number Found !! 🙂";
+    }
+    else if(guess > SecretNumber)
+    {
+        document.querySelector('.message').textContent = "Too High !😒";
+    }
+    else if(guess < SecretNumber)
+    {
+        document.querySelector('.message').textContent = "Too Low !😒";
+    }
+}) 
