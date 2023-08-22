993ecf39c6e2fae4055343dedf5e7f41a675fb70
krutik2377 <krutikgevariya7723@gmail.com>
2023-08-21 21:50:45 -0400

    Implemented again button functionality and highscore feature

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ document.querySelector('.score').textContent = 21;
 console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23; */
 
-const SecretNumber = Math.trunc(Math.random()*20) +1;
+let SecretNumber = Math.trunc(Math.random()*20) +1;
 let score = 20;
+let highscore = 0;
 
-document.querySelector('.number').textContent = SecretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
@@ -26,9 +26,15 @@ document.querySelector('.check').addEventListener('click', function() {
     {
         //when guess is matched.
         document.querySelector('.message').textContent = "Number Found !! 🙂";
+        document.querySelector('.number').textContent = SecretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
-
         document.querySelector('.number').style.width = '30rem';
+
+        if(score > highscore)
+        {
+            highscore = score;
+            document.querySelector('.highscore').textContent = score;
+        }
     }
     else if(guess > SecretNumber)
     {
@@ -63,3 +69,14 @@ document.querySelector('.check').addEventListener('click', function() {
         
     }
 }) 
+
+document.querySelector('.again').addEventListener('click', function() {
+    score = 20;
+    SecretNumber = Math.trunc(Math.random()*20) +1;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.guess').value = ' ';
+})
